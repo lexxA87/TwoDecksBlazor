3647f11f1ce8a26f3fc3802ce0e9f8d4dfdb67f3
lexxA87 <alex.o.nikonov@gmail.com>
2022-11-28 20:22:58 +0400

    Added buttons and select field to index.razor

--- TwoDecks.cs
+++ TwoDecks.cs
@@ -4,12 +4,12 @@
     {
         private Deck leftDeck = new Deck();
 
-        private int LeftDeckCount { get { return leftDeck.Count; } }
+        public int LeftDeckCount { get { return leftDeck.Count; } }
         public int LeftCardSelected { get; set; }
 
         public string LeftDeckCardName(int i)
         {
-            return leftDeck.ToString();
+            return leftDeck[i].ToString();
         }
 
         public void Shuffle()
