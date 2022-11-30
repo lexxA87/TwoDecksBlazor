b0e275d097698716c213681215cefcaf83fbacdc
lexxA87 <alex.o.nikonov@gmail.com>
2022-11-30 18:08:00 +0400

    Added right part: second field and buttons Clear and Sort

--- TwoDecks.cs
+++ TwoDecks.cs
@@ -2,14 +2,30 @@
 {
     public class TwoDecks
     {
-        private Deck leftDeck = new Deck();
+        private Deck leftDeck = new();
+        private Deck rightDeck = new();
 
         public int LeftDeckCount { get { return leftDeck.Count; } }
+        public int RightDeckCount { get { return rightDeck.Count; } }
         public int LeftCardSelected { get; set; }
+        public int RightCardSelected { get; set; }
 
         public string LeftDeckCardName(int i)
         {
             return leftDeck[i].ToString();
+        }
+        public string RightDeckCardName(int i)
+        {
+            return rightDeck[i].ToString();
+        }
+
+        public void Sort()
+        {
+            rightDeck.Sort(new CardCompererByValue());
+        }
+        public void Clear()
+        {
+            rightDeck.Clear();
         }
 
         public void Shuffle()
